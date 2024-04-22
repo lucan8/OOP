8b8d48b98d0d189c7e63db73b4f4616a4a2f57d2
lucan8 <gamer.lucan72@gmail.com>
2024-04-22 11:18:02 +0300

    added clone, I/O functions,  implemented train and getOVR

--- classes/players/player/Player.cpp
+++ classes/players/player/Player.cpp
@@ -1,29 +1,109 @@
 #include "Player.h"
 
+
+unique_ptr<Player> Player :: clone() const{
+    return make_unique<Player>(*this);
+}
+
+
+double Player :: getOVR() const{
+    double OVR = 0;
+    
+    const vector<pair<string, uint16_t>>& stats_ratios = Constants :: getStatsRatios(this->position);
+    const vector<uint16_t> ratios = getValues(stats_ratios);
+    uint16_t weights_sum = reduce(ratios.begin(), ratios.end());
+
+    for (const auto& stat_ratio : stats_ratios){
+        OVR += this->stats.at(stat_ratio.first) * stat_ratio.second;
+    }
+
+    return OVR / weights_sum;
+}
+
+void Player :: initStats(){
+    for (const auto& stat_name : Constants :: getStats("OUTFIELD"))
+        this->stats[stat_name];
+}
+
+void Player :: print(ostream& out) const{
+    this->printBasicInfo(out);
+    this->printSeasonStats(out);
+    this->printStats(out);
+}
+
+
+void Player :: printSeasonStats(ostream& out) const{
+    out << "Season Stats:\n"
+    << "Goals: " << this->s_goals 
+    << "\nAssists: " << this->s_assists << "\nYeallow Cards: " << this->s_yellow_cards
+    <<"\nRed Cards: " << this->s_red_cards;
+}
+
+void Player :: printStats(ostream& out) const{
+    out << "Player Stats:\n";
+    for (const auto& stat : stats)
+        out << stat.first << ": " << stat.second << '\n';
+    
+    out << "Potential: " << this->potential_OVR << '\n';
+    out << "OVR: " << this->getOVR() << '\n';
+}
+
+void Player :: printBasicInfo(ostream& out) const{
+    out << "Player Information:\n" 
+    << (Human&) (*this) << "Position: " << this->position
+    << "\nShirt number: " << this->shirt_nr << "\nStamina: " << this->stamina << "\n"
+    << "\nRed Carded: " << boolalpha << (this->red_carded)
+    << "\nTransfer eligible: " << boolalpha << this->transfer_eligible
+    << "\nForm: " << this->form << "/10\n";
+}
+
+
+void Player :: read(istream& in){
+    in >> (Human&)(*this) >> this->position >> this->shirt_nr >> this->potential_OVR;
+    this->readStats(in);
+}
+
+
 void Player :: resetSeasonStats(){
     s_goals = s_assists = s_yellow_cards = s_red_cards = form = 0;
     stamina = (double)Constants :: getVal("MAX_STAMINA");
     transfer_eligible = true;
     red_carded = false;
 }
+
+
 void Player :: rest(){
     this->stamina = max((double)Constants::getVal("MAX_STAMINA"), 
                         this->stamina + Constants::getVal("REST_STAMINA_PLUS"));
 }
-istream& operator >>(istream& op, Player& P){
-    op >> (Human&)P >> P.position >> P.shirt_nr >> P.potential_OVR >> P.stats;
 
-    return op;
+
+void Player :: readStats(istream& in){
+    string stat_name;
+    double val;
+
+    for (int i = 0; i < stats.size(); ++i){
+        in >> stat_name >> val;
+        try{
+            stats.at(stat_name) = val;
+        } catch(out_of_range& e){
+            cout << "Error(readStats), ivalid stat_name: " << stat_name << '\n';
+        }
+    }
 }
 
-ostream& operator <<(ostream& op, const Player& P){
-    op << (Human&)P << "\nPosition: " << P.position << "\nShirt: "
-    << P.shirt_nr << "\nGoals: " << P.s_goals 
-    << "\nAssists: " << P.s_assists << "\nYeallow Cards: " << P.s_yellow_cards
-    <<"\nRed Cards: " << P.s_red_cards << "\nRed Carded: " << boolalpha << P.red_carded
-    << "\nForm: " << P.form << "/10\nOVR: " << P.getOVR()
-    << "\nPrice: " << P.getPrice() << "\n";
+void Player :: train(){
+    const vector<pair<string, uint16_t>>& stats_ratios = Constants :: getStatsRatios(this->position);
 
-    return op;
+    vector<uint16_t> probabilities = getValues(stats_ratios);
+    random_device rd;
+    mt19937 gen(rd());
 
+    discrete_distribution<> stat_distibution(probabilities.begin(), probabilities.end());
+    uniform_real_distribution<double> train_plus(0.0, 1.0);
+
+    for (int i = 0; i < Constants :: getVal("NR_TRAIN_STATS"); ++i){
+        const string& chosen_stat = stats_ratios[stat_distibution(gen)].first;
+        this->stats.at(chosen_stat) += train_plus(gen);
+    }
 }
--- classes/players/player/Player.h
+++ classes/players/player/Player.h
@@ -1,32 +1,34 @@
 #pragma once
 #include "../../human/Human.h"
-
-istream& operator >>(istream& op, unordered_map<string, double>& stats);
-
 class Player : public Human{
 protected:
     unsigned short s_goals = 0, s_assists = 0, s_yellow_cards = 0, s_red_cards = 0, form = 0;
     unsigned short shirt_nr, potential_OVR;
+
     double stamina = (double)Constants::getVal("MAX_STAMINA");
     string position;
     unordered_map<string, double> stats;
+
     bool transfer_eligible = true;
     bool red_carded = false;
 
+    void readStats(istream&);
+    virtual void initStats();
 public:
-    Player();
+    Player() { initStats();}
     Player(const string& name, unsigned short age, double wage, const string& nationality, const unordered_map<string, double>& stats, unsigned short shirt_nr, unsigned short potential_OVR, const string& position) 
     : Human(name, age, wage, nationality), stats(stats), shirt_nr(shirt_nr), 
       position(position), potential_OVR(potential_OVR){}
-    ~Player(){cout << "Player Destroyed" << '\n';}
+    virtual ~Player(){};
 
-    virtual void train();
+    void train();
     void rest();
-    virtual void score(){ s_goals ++;};
-    virtual void assist(){ s_assists ++;};
-    void resetSeasonStats();
-    virtual double getOVR() const;
-    virtual double getPrice() const;
+    //Write ratios for these
+    void score();
+    void assist();
+    virtual void resetSeasonStats();
+    double getOVR() const;
+    double getPrice() const;
 
     unsigned short getGoals() const{return s_goals;}
     unsigned short getAssists() const{return s_assists;}
@@ -48,6 +50,12 @@ public:
     void changeTranferEligible(){this->transfer_eligible = !this->transfer_eligible;}
     void changeRedCarded(){this->red_carded = !this->red_carded;}
 
-    friend ostream& operator <<(ostream&, const Player&);
-    friend istream& operator >>(istream&, Player&);
+    virtual unique_ptr<Player> clone() const;
+
+    void print(ostream&) const override;
+    virtual void printSeasonStats(ostream&) const;
+    void printStats(ostream&) const;
+    void printBasicInfo(ostream&) const;
+
+    void read(istream&) override;
 };

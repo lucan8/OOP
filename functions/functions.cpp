78ed3dbbe41637689afe31f64085dc9fd7e60833
lucan8 <gamer.lucan72@gmail.com>
2024-05-05 20:27:44 +0300

    added min2 and split

--- functions/functions.cpp
+++ functions/functions.cpp
@@ -39,3 +39,30 @@ std :: vector<val> getValues(const std :: vector<std :: pair<key, val>>& u_map){
         values.push_back(k_v.second);
     return values;
 }
+
+std :: pair<std :: string, std :: string> minStats2
+(std :: vector<std :: pair<std :: string, double>>& stats){
+    std :: pair<std :: string, double> min1("", Constants :: getVal("MAX_STATS")), 
+                                       min2("", Constants :: getVal("MAX_STATS"));
+
+    for (const auto& stat : stats)
+        if (stat.second < min1.second){
+            min2 = min1;
+            min1 = stat;
+        }
+        else if(stat.second < min2.second)
+            min2 = stat;
+    
+    return make_pair(min1.first, min2.first);
+}
+
+std :: vector<std :: string> split(const std :: string& str, char sep){
+    std :: istringstream str_stream(str);
+    std :: string split_elem;
+    std :: vector<std :: string> split_string;
+
+    while (getline(str_stream, split_elem, sep))
+        split_string.push_back(split_elem);
+
+    return split_string;
+}
--- functions/functions.h
+++ functions/functions.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <vector>
 #include <unordered_map>
+#include <string>
+#include "../classes/constants/Constants.h"
 
 template <typename key, typename val>
 vector<key> getKeys(const unordered_map<key, val>& u_map);
@@ -13,3 +15,7 @@ vector<key> getKeys(const vector<pair<key, val>>& u_map);
 
 template <typename key, typename val>
 vector<val> getValues(const vector<pair<key, val>>& u_map);
+
+pair<string, string> minStats2(const vector<pair<string, double>>& stats);
+
+vector<string> split(const string& str, char sep = ' ');

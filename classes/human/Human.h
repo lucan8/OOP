63b5a94fa63d7ccbcae9a092c03d59d18e19ae30
lucan8 <gamer.lucan72@gmail.com>
2024-05-13 18:57:14 +0300

    changed to uint16_t

--- classes/human/Human.h
+++ classes/human/Human.h
@@ -8,20 +8,20 @@
 class Human : public Printable, public Readable{
 protected:
     string name, nationality;
-    unsigned short age;
+    uint16_t age;
     double wage;
 public:
     Human(){}
 
     void Age(){age ++;}
     
-    unsigned short getAge() const{return age;}
+    uint16_t getAge() const{return age;}
     double getWage() const{return wage;}
     const string& getNationality() const{return nationality;}
     const string& getName() const{return name;}
 
     void setWage(double wage){this->wage = wage;}
-    void setAge(unsigned short age){this->age = age;}
+    void setAge(uint16_t age){this->age = age;}
     void setNationality(const string& nationality){this->nationality = nationality;}
     void setName(const string& name){this->name = name;}
 

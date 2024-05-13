c44cce5cc0797371f89c715f007459385295baf7
lucan8 <gamer.lucan72@gmail.com>
2024-05-13 18:56:37 +0300

    removed conversion

--- classes/exceptions/FileOpenException.h
+++ classes/exceptions/FileOpenException.h
@@ -7,6 +7,6 @@ private:
     std :: string file_name; 
 public:
     FileOpenException(std :: string file_name) : file_name(file_name){}
-    const char* what() const  noexcept{return (std :: string("Could not open ") + file_name).c_str();}
+    const char* what() const  noexcept{return ("Could not open " + file_name).c_str();}
 };
 

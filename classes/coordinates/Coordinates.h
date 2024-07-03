fddd237f3f7da4e45511222609e258805f2072ca
lucan8 <gamer.lucan72@gmail.com>
2024-07-03 10:49:50 +0300

    read and bool conversion added

--- classes/coordinates/Coordinates.h
+++ classes/coordinates/Coordinates.h
@@ -1,4 +1,9 @@
-struct Coordinates {
+#pragma once
+#include "../readable/Readable.hpp"
+struct Coordinates : public Readable{
     int x;
     int y;
+    Coordinates(int x, int y): x(x), y(y){}
+    void read(istream& in) override {in >> x >> y;}
+    operator bool()const{return x || y;}
 };

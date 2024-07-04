c0da041a925ee7dd8cdcb014bf1ecf9599192295
lucan8 <gamer.lucan72@gmail.com>
2024-07-04 10:17:10 +0300

    added arithmetic operators

--- classes/coordinates/Coordinates.h
+++ classes/coordinates/Coordinates.h
@@ -4,6 +4,10 @@ struct Coordinates : public Readable{
     int x;
     int y;
     Coordinates(int x, int y): x(x), y(y){}
-    void read(istream& in) override {in >> x >> y;}
+    //Due to the fact that the coordinates are used in a matrix, the x and y values are swapped
+    void read(std :: istream& in) override {in >> y >> x;}
     operator bool()const{return x || y;}
+    Coordinates operator-()const{return Coordinates(-x, -y);}
+    Coordinates operator+(const Coordinates& other)const{return Coordinates(x + other.x, y + other.y);}
+    Coordinates operator-(const Coordinates& other)const{return Coordinates(x - other.x, y - other.y);}
 };

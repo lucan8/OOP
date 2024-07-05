3dfc7045dc3903fd726041ed254fd9464ed2380b
lucan8 <gamer.lucan72@gmail.com>
2024-07-05 10:42:00 +0300

    added conversion functions for vectors of pointers, and cloning

--- functions/functions.h
+++ functions/functions.h
@@ -51,4 +51,36 @@ std :: vector<val> getValues(const std :: vector<std :: pair<key, val>>& u_map){
     return values;
 }
 
+//Takes a vector of pointers and returns a vector of shared pointers
+template <typename T>
+vector<shared_ptr<T>> toSharedVector(const vector<T*>& vec){
+    vector<shared_ptr<T>> shared_vec;
+    shared_vec.reserve(vec.size());
+
+    for (const auto ptr : vec)
+        shared_vec.emplace_back(ptr);
+    return shared_vec;
+}
+
+//Takes a vector of pointers and returns a vector of unique pointers
+template <typename T>
+vector<unique_ptr<T>> toUniqueVector(const vector<T*>& vec){
+    vector<unique_ptr<T>> unique_vec;
+    unique_vec.reserve(vec.size());
+
+    for (const auto ptr : vec)
+        unique_vec.emplace_back(ptr);
+    return unique_vec;
+}
+
+//Only works if T has a clone method
+template <typename T>
+vector<shared_ptr<T>> clonePtrVector(const vector<shared_ptr<T>>& vec){
+    vector<shared_ptr<T>> cloned_vec;
+    cloned_vec.reserve(vec.size());
+
+    for (const auto& ptr : vec)
+        cloned_vec.emplace_back(ptr->clone());
+    return cloned_vec;
+}
 std :: vector<std :: string> split(const std :: string& str, char sep = ' ');

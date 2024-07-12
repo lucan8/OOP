3ba63a4aa68513a045bc32927739ffd61ce5ab30
lucan8 <gamer.lucan72@gmail.com>
2024-07-12 17:16:45 +0300

    managed to draw pitch and two teams

--- app/app.cpp
+++ app/app.cpp
@@ -1,24 +1,197 @@
 #include <iostream>
 #include <string>
 #include <fmt/core.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 #include <filesystem>
 #include <fstream>
-#include "../classes/teams/match_team/MatchTeam.h"
+#include "../classes/constants/Constants.h"
+#include "../classes/teams/team/Team.h"
+#include "../random_generated/player_generation/generate_player.h"
 #include "../random_generated/team_generation/generate_team.h"
+#include "../classes/shader/Shader.h"
+#include "../classes/VBO/VBO.h"
+#include "../classes/VAO/VAO.h"
+#include "../classes/IBO/IBO.h"
+#include <chrono>
+
+//Undefined reff to VAO
+
+//Unfinished, main only for testing purposes
 //External library Dasmig(see forked directory)
 //If reading from file add invalidCountry, invalidAge, invalidPoz
 //For the moment only using random_generated players and teams
 
+#define shader_path filesystem::current_path().parent_path() / "resources" / "shaders"
+const float triangle_offset = 0.02f;
+
+players_coords normalize(const players_coords& p_coords){
+    players_coords normalized_coords;
+    float startx = -1.0;
+    float starty = 1.0;
+
+    float unity_x = 1.0 / (Constants :: getVal("GOAL_LINE_LENGTH") / 2);
+    float unity_y = 1.0 / (Constants :: getVal("TOUCHLINE_LENGTH") / 2);
+
+    for (const auto& [pos, coords] : p_coords){
+        Coordinates normalized(startx + coords.x * unity_x, starty - unity_y *coords.y);
+        normalized_coords[pos] = normalized;
+    }
+    return normalized_coords;
+}
+
+players_coords changeSide(const players_coords& p_coords){
+    players_coords new_coords;
+    float max_x = Constants :: getVal("GOAL_LINE_LENGTH");
+    float max_y = Constants :: getVal("TOUCHLINE_LENGTH");
+
+    for (const auto& [pos, coords] : p_coords)
+        new_coords[pos] = Coordinates(max_x - coords.x, max_y - coords.y);
+    return new_coords;
+}
+
+
+//Make triangle from the player's coordinates depending on the half
+float* getTriangle(const Coordinates& p_coords, bool second_half){
+    //triangl if the player is in second half(grows upwards)
+    if (second_half)
+        return new float[6]{
+            p_coords.x, p_coords.y,
+            p_coords.x - triangle_offset, p_coords.y + triangle_offset,
+            p_coords.x + triangle_offset, p_coords.y + triangle_offset
+        };
+    //triangle if the player is in first half(grows downwards)
+    return new float[6]{
+        p_coords.x, p_coords.y,
+        p_coords.x - triangle_offset, p_coords.y - triangle_offset,
+        p_coords.x + triangle_offset, p_coords.y - triangle_offset
+    };
+}
+
+
+void addTeamPositions(float* positions, int& positions_size,
+                      const players_coords& team_coords, bool second_half){
+    for (const auto& [pos, coords] : team_coords){
+        float* triangle = getTriangle(coords, second_half);
+        memcpy(positions + positions_size, triangle, 6 * sizeof(float));
+        positions_size += 6;
+        delete[] triangle;
+    }
+}
+
 int main(){
     Constants :: init();
-    vector<string> available_names = Constants::getTeamNames();
-    try{
-        shared_ptr<Team> t1 = generateTeam(available_names), t2 = generateTeam(available_names);
-        MatchTeam m_1(move(t1->getBestFirstTeam()), t1), m_2(move(t2->getBestFirstTeam()), t2);         
-        cout << "sus" << endl;
-    } catch(exception& e){
-        cout << e.what() << endl;
+    const vector<string> formations = Constants :: getFormationsNames();
+    const players_coords& team1 = Constants :: getPlayersCoords("4-4-2");
+    players_coords normalized_team1 = normalize(team1), normalized_team2 = normalize(changeSide(team1));
+
+    //Initializing glfw
+    if (!glfwInit()){
+        cout << "Failed to initialize GLFW\n";
     }
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    //Initializing window
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Hello World", NULL, NULL);
+    if (!window){
+        cout << "Failed to create window\n";
+        glfwTerminate();
+    }
+    //Creating context for window
+    glfwMakeContextCurrent(window);
+    
+    //Initializing glew
+    if (glewInit() != GLEW_OK){
+        cout << "Failed to initialize GLEW\n";
+    }
+
+    //8 for pitch, 4 middle line,  6(triangle) * 22 for players
+    float positions[8 + 4 + 6 * 22] = {
+        //Green square
+        -1.0f, -1.0f,
+        -1.0f, 1.0f,
+         1.0f, 1.0f,
+         1.0f, -1.0f,
+
+         //Middle line
+         -1.0f, 0.0f,
+          1.0f, 0.0f,
+    };
+    int positions_size = 12;
+    addTeamPositions(positions, positions_size, normalized_team1, true);
+    addTeamPositions(positions, positions_size, normalized_team2, false);
+
+    GLuint indices[6] = {
+        //For square(and triangle)
+        0, 1, 2,
+        //Used only for square
+        2, 3, 0,
+    };
+    VAO vao;
+    vao.bind();
+    //Already bound on creation
+    VBO vbo(positions, sizeof(positions));
+    IBO ibo(indices, 6 * sizeof(GLuint));
+
+    Shader pitch_shader((shader_path / "vertex_shader.glsl").string(),
+                    (shader_path / "fragment_shader.glsl").string()),
+           middle_line_shader((shader_path / "vertex_shader.glsl").string(),
+                    (shader_path / "fragment_shader2.glsl").string()),
+           player_shader((shader_path / "vertex_shader.glsl").string(),
+                    (shader_path / "fragment_shader1.glsl").string());
+    
+    while (!glfwWindowShouldClose(window)){
+        glClear(GL_COLOR_BUFFER_BIT);
+        //Drawing the pitch
+        pitch_shader.use();
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+
+        //Drawing the middle line
+        middle_line_shader.use();
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, (void*)(8 * sizeof(float)));
+        glDrawElements(GL_LINES, 2, GL_UNSIGNED_INT, nullptr);
+        
+
+        //Drawing the players
+        player_shader.use();
+        for (int i = 0; i < Constants :: getVal("MATCH_TEAM_SIZE") * 2; ++i){
+            glEnableVertexAttribArray(0);
+            glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, (void*)((8 + 4 + i * 6) * sizeof(float)));
+            glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr);
+        }
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    
+    // Constants :: init();
+    // vector<string> team_names = Constants :: getTeamNames();
+    // vector<unique_team> teams;
+    // teams.reserve(100);
+    // auto a = chrono :: high_resolution_clock::now();
+
+    // for (int i = 0; i < 20; ++i)
+    //     teams.push_back(unique_team(generateTeam(team_names)));
+    
+    // //test this
+    // for (const auto& t : teams){
+    //     shared_squad_split split_team = t->splitTeamPos();
+    //     printSplitTeam(split_team);
+    //     unique_first_team first_team = t->getFirstTeam();
+    //     cout << first_team->getFormation() << ", "
+    //          << first_team->getElevenTotalStats() << ", "
+    //          << first_team->getSubsOVR() << '\n';
+    //     cout << endl << endl;
+    // }
+    // auto b = chrono :: high_resolution_clock::now();
+
+    // cout << "took " << duration_cast<chrono :: seconds>(b - a).count() << " seconds" <<  endl;
+
 }
 
 
@@ -129,9 +302,25 @@ void trainTeams50(const vector<shared_ptr<Team>>& Teams){
     for (auto& t : Teams)
         training50(t->getPlayers());
 }
+
+double getAvgOVR(const shared_squad& squad){
+    double ovr = 0;
+    for (const auto& p : squad)
+        ovr += p->calculateOVR(p->getPosition());
+    return ovr / squad.size();
+}
+void printSplitTeam(const shared_squad_split& split_team){
+    for (const auto& p : split_team)
+        cout << p.first << "(" << p.second.size() << "): " << getAvgOVR(p.second) << "\n";
+}
+
+void printStats(const unordered_map<string, double>& stats){
+    vector<string> outfield_stats = Constants :: getStats("OUTFIELD");
+    for (const auto& s : stats)
+        if (find(outfield_stats.begin(), outfield_stats.end(), s.first )
+            != outfield_stats.end())
+        cout << s.first << ": " << s.second << '\n';
+}
 */
-
-
-
 
 

dfabfa3f9c30f65609ad22fb6b8c19f9bb59f527
lucan8 <gamer.lucan72@gmail.com>
2024-07-22 09:03:08 +0300

    using projection matrix

--- resources/shaders/vertex/player.glsl
+++ resources/shaders/vertex/player.glsl
@@ -3,7 +3,9 @@ layout(location = 0) in vec4 vertexPosition;
 layout(location = 1) in vec2 texCoords;
 
 out vec2 v_texCoords;
+uniform mat4 u_MVP;
+
 void main(){
-    gl_Position = vertexPosition;
+    gl_Position = vertexPosition * u_MVP;
     v_texCoords = texCoords;
 }

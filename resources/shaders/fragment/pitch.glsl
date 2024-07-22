90ac4530d9dff6c955ff69a4981ea6cf154288ff
lucan8 <gamer.lucan72@gmail.com>
2024-07-22 09:03:52 +0300

    now draws texture

--- resources/shaders/fragment/pitch.glsl
+++ resources/shaders/fragment/pitch.glsl
@@ -1,8 +1,11 @@
 #version 330 core
 
 out vec4 fragColor;
+in vec2 v_texCoords;
 
-uniform vec4 u_Color;
+uniform sampler2D u_Texture;
+
 void main(){
-    fragColor = u_Color;
+    vec4 texColor = texture(u_Texture, v_texCoords);
+    fragColor = texColor;
 }

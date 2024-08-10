9a35090aa6e3236cc5ce79e4c884601026a04c24
lucan8 <gamer.lucan72@gmail.com>
2024-08-10 11:21:15 +0300

    fixed bug

--- classes/exceptions/MyRuntimeException.h
+++ classes/exceptions/MyRuntimeException.h
@@ -5,7 +5,7 @@ private:
     std :: string message;
 public:
     MyRuntimeException(const std :: string& curr_file, const std :: string& curr_func,
-                       int curr_line, std :: string stat_name)
+                       int curr_line, std :: string message)
      : MyException(curr_file, curr_func, curr_line), message(message){}
     const char* what() const  noexcept{
         return ((std :: string)MyException :: what() + message).c_str();

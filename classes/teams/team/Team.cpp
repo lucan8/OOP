c4a07d2709f73852aaad74a067e841484a6cba2f
lucan8 <gamer.lucan72@gmail.com>
2024-08-13 10:45:13 +0300

    moved first team building functions, added aura color

--- classes/teams/team/Team.cpp
+++ classes/teams/team/Team.cpp
@@ -8,157 +8,12 @@ team_ptr Team :: clone() const{
     return new Team(*this);
 }
 
-unique_first_team Team :: getFirstTeam() const{
-    unique_first_team best_first_team;
-    for (const auto& f_name : Constants :: getFormationsNames()){
-        
-        //Splitting the team by player type
-        shared_squad_split split_team  = splitTeamPType();
-        //Creating a map for the unsused outfield players
-        shared_squad_map unused_outfields = toUMap(split_team.at("OUTFIELD"));
-        //Creating a map for the unsused goalkeepers
-        shared_squad_map unused_goalkeepers = toUMap(split_team.at("GK"));
-        
-        //This needs to execute before getSubstitutes
-        //because it removes players from the unused players
-        shared_m_squad first_eleven = this->getFirstEleven(f_name, unused_outfields, unused_goalkeepers);
-        unique_first_team first_team(new FirstTeam(f_name,
-                                                   move(first_eleven),
-                                                   this->getSubstitutes(unused_outfields, unused_goalkeepers)
-                                                   ));
-        if (best_first_team == nullptr || first_team->getElevenTotalStats() > best_first_team->getElevenTotalStats())
-            best_first_team = move(first_team);
-    }
-    return best_first_team;
+
+void Team :: setAuraColor(const glm :: vec4& color){
+    aura_color = normalizeColor(color); 
+    aura_color.a = 0.5f;
 }
 
-shared_m_squad Team :: getFirstEleven(const string& form_name, shared_squad_map& unused_outfields,
-                                      shared_squad_map& unused_goalkeepers){
-    shared_m_squad first_eleven = getFirstTeamOutfields(form_name, unused_outfields);
-    first_eleven.emplace_back(getBestGoalkeeper(unused_goalkeepers));
-
-    return first_eleven;
-}
-
-
-shared_m_squad Team :: getFirstTeamOutfields(const string& form_name, shared_squad_map& unused_outfields){
-    shared_m_squad first_eleven;
-    first_eleven.reserve(Constants :: getVal("MATCH_TEAM_SIZE"));
-
-    for (const auto& m_pos : Constants :: getFormationPositions(form_name)){
-        //Getting the match position equivalence(we can't calculate OVR without it)
-        const string& pos = Constants :: getPosEquivalence(m_pos);
-        //Getting the best player for that position
-        uint16_t best_index = Team :: getBestPlayerIndex(unused_outfields, pos);
-        shared_player& best_player = unused_outfields.at(best_index);
-        
-        //Adding the player to the first eleven
-        first_eleven.emplace_back(new MatchOutfield(best_player, m_pos, best_player->calculateOVR(pos),
-                                                    Constants :: getMPosCoords(m_pos)));
-        //removing the player from the unused players
-        unused_outfields.erase(best_index);
-    }
-    return first_eleven;
-}
-
-
-m_player_ptr Team :: getBestGoalkeeper(shared_squad_map& unused_goalkeepers){
-    uint16_t best_gk_index = Team :: getBestPlayerIndex(unused_goalkeepers, "GK");
-    shared_player& chosen_gk = unused_goalkeepers.at(best_gk_index);
-
-    m_player_ptr best_gk = new MatchGoalkeeper(chosen_gk, "GK", chosen_gk->calculateOVR("GK"), 
-                                                Constants :: getMPosCoords("GK"));
-    unused_goalkeepers.erase(best_gk_index);
-
-    return best_gk;
-}
-
-
-uint16_t Team :: getBestPlayerIndex(const shared_squad_map& players, const string& pos){
-    float best_OVR = 0;
-    uint16_t best_p_index = 0;
-
-    //Going through all players for that position(first: index, second: player)
-    for (const auto& player : players){
-        float p_ovr = player.second->calculateOVR(pos);
-         //Keeping track of the index of the best player
-        if (p_ovr > best_OVR){
-            best_OVR = p_ovr;
-            best_p_index = player.first;
-        }
-    }
-
-    return best_p_index; 
-}
-
-
-unique_m_squad Team :: getSubstitutes(shared_squad_map& unused_outfields, shared_squad_map& unused_goalkeepers){
-    unique_m_squad substitutes(Constants :: getVal("NR_PLAYERS_BENCH"));
-    substitutes[0] = unique_m_player(getBestGoalkeeper(unused_goalkeepers));
-    uint16_t subs_index = 1;
-
-    //Going through all the p_types for the substitutes
-    for (const auto& ptype_pnr : Constants :: getSubsLayout()){
-        //Skipping the GK p_type
-        if (ptype_pnr.first == "GK")
-            continue;
-        //The same players, but now in the form of match players with the best positions
-        //related to the detailed player type
-        unique_m_squad_map players = getSubsOutfields(unused_outfields, ptype_pnr.first);
-        //Getting the best two players for that p_type
-        pair<uint16_t, uint16_t> best_players = getMax2PlayersIndexes(players);
-
-        //Adding the players to the substitutes
-        substitutes[subs_index++] = move(players.at(best_players.first));
-        substitutes[subs_index++] = move(players.at(best_players.second));
-
-        //Removing the players from the unused players
-        unused_outfields.erase(best_players.first);
-        unused_outfields.erase(best_players.second);
-    }
-    return substitutes;
-}
-
-//TO DO:Name differently
-unique_m_squad_map Team :: getSubsOutfields(shared_squad_map& unused_outfields, const string& det_p_type){
-    unique_m_squad_map possible_substitutes;
-
-    possible_substitutes.reserve(unused_outfields.size());
-
-    //Going through all the outfield players(player.first is index, player.second is the actual player)
-    for (const auto& player : unused_outfields){
-        float best_OVR = 0;
-        string best_pos;
-        //Going through all the positions related to the detailed player type for that player
-        for (const auto& pos : Constants :: getPositions(det_p_type)){
-            float p_ovr = player.second->calculateOVR(pos);
-            //Choosing the best position for that player
-            if (p_ovr > best_OVR){
-                best_OVR = p_ovr;
-                best_pos = pos;
-            }
-        }
-        possible_substitutes.insert(make_pair(player.first, unique_m_player(new MatchOutfield(player.second, best_pos, best_OVR))));
-    }
-
-    return possible_substitutes;
-}
-pair<uint16_t, uint16_t> Team :: getMax2PlayersIndexes(const unique_m_squad_map& players){
-    //first is OVR, second is index
-    pair<float, uint16_t> best_player1(0, 0), best_player2(0, 0);
-
-    for (const auto& player : players){
-        float p_ovr = player.second->getOVR();
-        if (p_ovr > best_player1.first){
-            best_player2 = best_player1;
-            best_player1 = make_pair(p_ovr, player.first);
-        }
-        else if (p_ovr > best_player2.first)
-            best_player2 = make_pair(p_ovr, player.first);
-        }
-        
-    return make_pair(best_player1.second, best_player2.second);
-}
 
 shared_squad_split Team :: splitTeamPos() const{
     unordered_map<string, shared_squad> split_team;
@@ -170,19 +25,6 @@ shared_squad_split Team :: splitTeamPos() const{
     return split_team;
 }
 
-
-shared_squad_split Team :: splitTeamPType() const{
-    shared_squad_split split_team;
-    initMap(split_team, Constants :: getPlayerTypes());
-
-    for (const auto& p : this->Players)
-        if (p->getPosition() != "GK")
-            split_team.at("OUTFIELD").push_back(p);
-        else    
-            split_team.at("GK").push_back(p);
-
-    return split_team;
-}
 
 
 void Team :: resetSeasonStats(){
--- classes/teams/team/Team.h
+++ classes/teams/team/Team.h
@@ -1,6 +1,8 @@
 #pragma once
-#include "../../first_team/FirstTeam.h"
 #include "../../../functions/functions.h"
+#include "../../players/outfield/OutFieldPlayer.h"
+#include "../../players/goalkeeper/Goalkeeper.h"
+#include <glm/glm.hpp>
 //Add comparator for players and way to retrieve e a sorted vector of players
 
 class Team;
@@ -15,33 +17,8 @@ private:
     float budget;
     shared_squad Players;
     uint16_t points = 0;
-
-    //Should only be used before getSubstitutes
-    //Returns the best first eleven for a formation from unused players
-    //Players that are part of the starting 11 are removed from the unused players
-    static shared_m_squad getFirstEleven(const string& form_name, shared_squad_map& unused_outfields,
-                                         shared_squad_map& unused_goalkeepers);
-
-    //Helper function for getFirstEleven, returns the best outfield players for a formation
-    static shared_m_squad getFirstTeamOutfields(const string& form_name, 
-                                                shared_squad_map& unused_outfields);
-                                                
-    //Returns the best goalkeeper in the form of a match player
-    static m_player_ptr getBestGoalkeeper(shared_squad_map& unused_goalkeepers);
-
-    //Should only be used after getFirstEleven
-    //Returns the best substitutes from unused players
-    //Players that are part of the substitutes are removed from the unused players                                     
-    static unique_m_squad getSubstitutes(shared_squad_map& unused_outfields, shared_squad_map& unused_goalkeepers);
-
-    //Helper function for getSubstitutes, returns the best outfield substitutes
-    static unique_m_squad_map getSubsOutfields(shared_squad_map& unused_outfields, const string& det_p_type);
-
-    //Helper function for getSubsOutfields, returns the best two match players indexes
-    static pair<uint16_t, uint16_t> getMax2PlayersIndexes(const unique_m_squad_map& players);
+    glm :: vec4 aura_color;
     
-    //Splits team by player type(OUTFIELD, GK)
-    shared_squad_split splitTeamPType() const;
 public:
     shared_squad_split splitTeamPos() const;
     Team(const string& name = "", shared_squad Players = {}, float budget = 0) : 
@@ -57,11 +34,12 @@ public:
     unsigned short getPoints() const{return points;}
     const shared_squad& getPlayers() const{return Players;}
 
+    const glm :: vec4& getAuraColor() const{return aura_color;}
+    //Sets the aura color for the players, the color is normalized, alpha is 0.5
+    void setAuraColor(const glm :: vec4& color);
+
     //Returns a map of averages calculated from the players stats
     unordered_map<string, float> getTeamStats() const;
-    unique_first_team getFirstTeam() const;          
-    //Returns the index of the best player for a position                    
-    static uint16_t getBestPlayerIndex(const shared_squad_map& players, const string& pos);
     team_ptr clone() const; 
 
     void setBudget(float budget){this->budget = budget;}

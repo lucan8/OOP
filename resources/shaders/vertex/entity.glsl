cbe0857243907ec577cf87d8a2e40d99fa53ffbe
lucan8 <gamer.lucan72@gmail.com>
2024-09-23 12:09:38 +0300

    added the model matrix

--- resources/shaders/vertex/entity.glsl
+++ resources/shaders/vertex/entity.glsl
@@ -4,8 +4,8 @@ layout(location = 1) in vec2 texCoords;
 
 out vec2 v_texCoords;
 uniform mat4 u_projection;
-
+uniform mat4 u_model;
 void main(){
-    gl_Position = u_projection * vertexPosition;
+    gl_Position = u_projection * u_model * vertexPosition;
     v_texCoords = texCoords;
 }

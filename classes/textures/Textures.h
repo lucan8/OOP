24f14388067f67aa4b747de3d790698d190076c1
lucan8 <gamer.lucan72@gmail.com>
2024-09-25 10:53:24 +0300

    removed ability to copy or moved, removed slot system

--- classes/textures/Textures.h
+++ classes/textures/Textures.h
@@ -6,16 +6,14 @@
 #include <memory>
 //TODO: Have options without mipmaps
 //Slots are set during creation of the texture(will always be slots_in_use + 1)
+class Textures;
+typedef std :: unique_ptr<Textures> unique_texture;
 class Textures{
 private:
     GLuint id;
-    int slot;
-    static uint16_t slots_in_use;
     std :: unique_ptr<GLubyte> localBuffer;
     GLint width, height, channel_count;
 
-    //Sets and binds the texture to the slot
-    void setSlot(int slot);
     void setParams() const;
     //Loads the texture from the file path(used for images)
     void load(const std :: string& file_path);
@@ -26,20 +24,18 @@ public:
     Textures(const std :: string& file_path);
     //Creates a texture from a buffer(used for fonts)
     Textures(const GLubyte* buffer, int width, int height);   
-    //Deep copy constructor
-    Textures(const Textures& other);
-    //Move constructor
-    Textures(Textures&& other);
-    //Can be used for deep copy and move
-    Textures& operator=(Textures other);
+    //Two textures can't have the same id(if one gets deleted, the other will be invalid)
+    Textures(const Textures& other) = delete;
+    //Moving means we now have a texture with the same id as the other, which is invalid
+    Textures(Textures&& other) = delete;
+    
+    Textures& operator=(const Textures& other) = delete;
     ~Textures();
 
-    void bind() const;
-    void unbind();
-    GLuint getSlot() const{return this->slot;}
+    void bind(uint16_t slot) const;
+    void unbind() const;
 
     GLuint get() const{return this->id;}
     //Returns the average color of the texture(not normalized, values from 0 to 255)
     glm :: vec4 getAverageColor() const;
-    friend void swap(Textures& first, Textures& second);
 };
